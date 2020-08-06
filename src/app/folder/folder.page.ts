9721e82383e95e47b652e9551702dda99a499c7f
Dallas James <dallastjames@gmail.com>
2020-08-06 14:56:39 -0600

    create demo

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private menuController: MenuController,
+  ) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  public async menu(type: string): Promise<void> {
+    const matchedMenu = await this.menuController.enable(true, type);
+    await this.menuController.open(type);
+    console.log(matchedMenu);
+  }
 }
